707a2932b7fd70fdcf4317139399770556908767
unknown <ibmjavanh19@iiht.tech>
2021-04-19 04:17:17 +0530

    pushed implementation of create bug

---
+++ src/app/create-bug/create-bug.component.ts
@@ -0,0 +1,37 @@
+
+import { Component, OnInit } from '@angular/core';
+import { BugService } from '../bug.service';
+import { Bug } from '../Bug';
+
+
+@Component({
+  selector: 'app-create-bug',
+  templateUrl: './create-bug.component.html',
+  styleUrls: ['./create-bug.component.css']
+})
+export class CreateBugComponent implements OnInit {
+  title: String = 'CreateBug';
+  bug: Bug = new Bug();
+
+
+  constructor(private bugService: BugService) { }
+  saveBug() {
+    const promise = this.bugService.saveBug(this.bug);
+    promise.subscribe(response => {
+      console.log(response);
+      alert("Bug saved...");
+    },
+      error => {
+        console.log(error);
+        alert(error.statusText);
+
+      }
+    )
+
+
+  }
+  ngOnInit(): void {
+
+  }
+
+}

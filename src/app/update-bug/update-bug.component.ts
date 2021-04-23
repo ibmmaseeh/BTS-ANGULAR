d81a786f13f86d72fe35f30581a461fdff61e4b2
unknown <ibmjavanh19@iiht.tech>
2021-04-23 16:29:06 +0530

    Validated Update Status flow in the front end

--- src/app/update-bug/update-bug.component.ts
+++ src/app/update-bug/update-bug.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { BugService } from '../bug.service';
 import { Bug } from '../Bug';
+import { STATUS } from "../STATUS";
 
 @Component({
   selector: 'app-update-bug',
@@ -9,6 +10,7 @@ import { Bug } from '../Bug';
 })
 export class UpdateBugComponent implements OnInit {
   //title: String = 'BugForm';
+  oldStatus: string;
   bug: Bug = new Bug();
   bugList:any;
   constructor(private bugService: BugService) { }
@@ -23,7 +25,8 @@ export class UpdateBugComponent implements OnInit {
         console.log(this.bugList);
         if(this.bugList){
           this.bug=this.bugList;
-          let resEtaDate = this.bug.etaDate;
+           this.oldStatus= this.bugList.status;
+           let resEtaDate = this.bug.etaDate;
           let resSubmitDate=this.bugList.submitOnDate;
           if (resSubmitDate) {
             let finalSubmitDate = resSubmitDate.split('T')[0];
@@ -71,12 +74,65 @@ else{
       etaDate: (<HTMLInputElement>document.getElementById('etaDate')).value,
     }
 
+
     this.bugService.updateBug(bugId, updatedBody).subscribe(
       response => {
         console.log(response);
-        alert("Bug updated!");
+        this.bugList= response;
+        console.log(response);
+        console.log(updatedBody.status);
+        alert('Bug Updated');
+
+
       },
+
       error => {
+        if( this.oldStatus== 'NEW' && updatedBody.status!='ASSIGNED'){
+          alert('Status not allowed, NEW bug can only be assigned.');
+          return;
+        }
+        else if (this.oldStatus== 'ASSIGNED' && updatedBody.status=='NEW'){
+          alert('Assigned bug cannot be updated to status NEW.');
+          return;
+
+        }
+        else if (this.oldStatus=='OPEN' && (updatedBody.status=='NEW'||updatedBody.status=='ASSIGNED')){
+          alert('An OPEN bug cannot have updated status as NEW or ASSIGNED,');
+          return;
+        }
+        else if(this.oldStatus=='FIXED' && (updatedBody.status=='OPEN'||updatedBody.status=='NEW'||updatedBody.status=='ASSIGNED')){
+          alert('FIXED bug cannot have updated status as NEW or OPEN or ASSIGNED, please try REOPENING It.  ');
+          return;
+        }
+        else if(this.oldStatus=='PENDING_RETEST' && (updatedBody.status=='FIXED'||updatedBody.status=='OPEN'||updatedBody.status=='NEW'||updatedBody.status=='ASSIGNED')){
+          alert('A bug in PENDING RETEST status cannot be FIXED or NEW or OPEN or ASSIGNED ');
+          return;
+        }
+
+        else if(this.oldStatus=='RETEST' && (updatedBody.status=='PENDING_RETEST'||updatedBody.status=='FIXED'||updatedBody.status=='OPEN'||updatedBody.status=='NEW'||updatedBody.status=='ASSIGNED')){
+           alert('A bug in RETEST cannot be PEDNING RETEST or FIXED or OPEN or NEW or ASSIGNED');
+           return;
+
+        }
+
+        else if(this.oldStatus=='REOPEN' && (updatedBody.status=='CLOSED'||updatedBody.status=='VERIFIED'||updatedBody.status=='OPEN'||updatedBody.status=='NEW'||updatedBody.status=='ASSIGNED')){
+          alert('A bug thats REOPEN cannot be CLOSED or VERFIED or OPEN or NEW or ASSIGNED');
+          return;
+        }
+
+        else if(this.oldStatus=='VERIFIED' && (updatedBody.status=='REOPEN'||  updatedBody.status=='RETEST'||updatedBody.status=='PENDING_RETEST'||updatedBody.status=='FIXED'||updatedBody.status=='OPEN'||updatedBody.status=='NEW'||updatedBody.status=='ASSIGNED')){
+          alert('A bug thats VERIFIED cannot have its status updates as REOPEN OR RETETST OR PENDING_RETEST OR FIXED or OPEN or NEW or ASSIGNED');
+          return;
+        }
+
+
+        else if(this.oldStatus=='CLOSED' && (updatedBody.status=="VERIFIED"||updatedBody.status=='REOPEN'||  updatedBody.status=='RETEST'||updatedBody.status=='PENDING_RETEST'||updatedBody.status=='FIXED'||updatedBody.status=='OPEN'||updatedBody.status=='NEW'||updatedBody.status=='ASSIGNED')){
+          alert('A CLOSED bug cannot cannot be updated to status of VERIFIED or REOPEN or RETEST or PENDING RETEST, FIXED or OPEN or NEW or ASSIGNED');
+          return;
+        }
+
+
+
         console.log(error);
         alert("Error Happened!");
 
